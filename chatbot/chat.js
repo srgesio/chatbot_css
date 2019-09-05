4fa6c60645fc7afaa08b3badc8920914f897ebe3
Gésio de Moura <gesio.m@escolar.ifrn.edu.br>
2019-09-05 17:28:07 -0300

    Problemasresolvidos

--- chatbot/chat.js
+++ chatbot/chat.js
@@ -1,1 +1,7 @@
-var elmnt = document.getElementsByClassName(".chat-body");
+function scrollDiv() {
+    var el;
+    if ((el = document.getElementsByClassName('chat-body')) && ('undefined' != typeof el.scrollTop)) {
+        el.scrollTop = 0;
+        el.scrollTop = 5000;
+    }
+}
